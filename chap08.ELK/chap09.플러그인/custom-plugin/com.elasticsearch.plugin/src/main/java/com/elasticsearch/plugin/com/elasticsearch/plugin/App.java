e53183c3b55b1d23e6c56ce08100321c2f118eb8
leejm1 <wnsaud6016@naver.com>
2020-05-19 00:16:48 +0900

    init

---
+++ chap08.ELK/chap09.플러그인/custom-plugin/com.elasticsearch.plugin/src/main/java/com/elasticsearch/plugin/com/elasticsearch/plugin/App.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.plugin.com.elasticsearch.plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
