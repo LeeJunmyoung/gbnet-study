4748e001e179b8f4bd932808d1d8f409cdd40746
leejm1 <leejm1@gbnet.kr>
2020-05-29 17:38:51 +0900

    plugin init

---
+++ chap08.ELK/chap09.플러그인/custom-plugin/ElasticsearchPlugin/src/main/java/com/elasticsearch/plugin/PluginApp.java
@@ -0,0 +1,15 @@
+package com.elasticsearch.plugin;
+
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.Plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class PluginApp extends Plugin implements ActionPlugin
+{
+	public static void main (String args[]) {
+		System.out.println("test");
+	}
+}
