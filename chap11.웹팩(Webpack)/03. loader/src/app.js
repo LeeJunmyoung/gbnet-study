3ad75fa16e49469c8ba1e8454959ace59ea8abfe
leejm1 <leejm1@gbnet.kr>
2020-06-30 09:30:56 +0900

    css-loader source

--- chap11.웹팩(Webpack)/03. loader/src/app.js
+++ chap11.웹팩(Webpack)/03. loader/src/app.js
@@ -1,2 +1,3 @@
+import './style.css';
 const math = require('./math.js');
 console.log(math.sum(1,2));
--- chap11.웹팩(Webpack)/03. loader/webpack.config.js
+++ chap11.웹팩(Webpack)/03. loader/webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
     rules: [{
         test: /\.js$/, // .js 확장자로 끝나는 모든 파일에 적용
         use: [path.resolve('./src/myloader.js')] // 커스텀 로더
+    },{
+      test: /\.css$/, // .css 확장자로 끝나는 모든 파일 
+      use: ['css-loader'], // css-loader를 적용한다 
     }],
   }
 }
