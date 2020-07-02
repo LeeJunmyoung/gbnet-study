a95f105b040f94e6c9762f6b0ff970a4341fa1bf
leejm1 <wnsaud6016@naver.com>
2020-07-02 23:45:35 +0900

    file-loader

--- chap11.웹팩(Webpack)/03. loader/webpack.config.js
+++ chap11.웹팩(Webpack)/03. loader/webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
     },{
       test: /\.css$/, // .css 확장자로 끝나는 모든 파일 
       use: ['style-loader', 'css-loader'], // style-loader, css-loader를 적용한다. style-loader는 앞에 적용.
+    },{
+      test: /\.(png|jpe?g|gif)$/i, // .png 확장자로 마치는 모든 파일
+      loader: 'file-loader',
+      options: {
+        publicPath: './dist/', // prefix를 아웃풋 경로로 지정 
+        name: '[name].[ext]?[hash]', // 파일명 형식 
+      }
     }],
   }
 }
